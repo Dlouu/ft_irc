c053263ff4a4b87a8d953101db43d234ab424013
Dlouu <135530996+Dlouu@users.noreply.github.com>
2025-08-14 15:33:49 +0200

    Dlou: 14/08/25: NumReplies gros bordel

--- Server/NumReply.hpp
+++ Server/NumReply.hpp
@@ -2,6 +2,7 @@
 
 #include <map>
 #include <string>
+#include <sstream>
 #include <iostream>
 #include <algorithm>
 #include <netinet/in.h>
@@ -17,9 +18,48 @@ typedef enum	e_NumReply {
 	ERR_NICKNAMEINUSE		= 433
 }	t_NumReply;
 
-static const std::map<int, std::string> replyTemplates = {
-    { 1,  ":{server} 001 {nick} :Welcome to the IRC Network {nick}!{user}@{host}\r\n" },
-    { 431,  ":{server} 431 :No nickname given\r\n" },
-    { 432,  ":{server} 432 {nick} :Erroneus nickname\r\n" },
-    { 433,  ":{server} 433 {nick} :Nickname is already in use\r\n" },
-};
+static std::map<int, std::string> createReplies() {
+    std::map<int, std::string> num;
+
+    num[RPL_WELCOME] =			":{server} 001 {nick} :Welcome to the IRC Network {nick}!{user}@{host}\r\n";
+    num[ERR_NONICKNAMEGIVEN] =	":{server} 431 {nick} :No nickname given\r\n";
+	num[ERR_ERRONEUSNICKNAME] =	":{server} 432 {nick} :Erroneous nickname\r\n";
+    num[ERR_NICKNAMEINUSE] =	":{server} 433 {nick} :Nickname is already in use\r\n";
+    return num;
+}
+
+static const std::map<int, std::string> replies = createReplies();
+
+static std::map<std::string, std::string> fillVars() {
+    std::map<std::string, std::string> vars;
+
+    vars["nick"] =		" NICKNAME ";
+    vars["user"] =		" USERNAME ";
+	vars["server"] =	" SERVER ";
+    vars["host"] =		" HOST ";
+    return vars;
+}
+
+static const std::map<std::string, std::string> vars = fillVars();
+
+std::string formatReply(int code, const std::map<std::string, std::string> &vars) {
+	std::map<int, std::string>::const_iterator templateIt = replies.find(code);
+	if (templateIt == replies.end())
+        return "";
+	std::string reply = templateIt->second;
+
+	std::map<std::string, std::string>::const_iterator it;
+	//std::string reply = it->second;
+    for (it = vars.begin(); it != vars.end(); ++it) {
+        std::string key = "{" + it->first + "}";
+        std::string::size_type pos = 0;
+        
+        while ((pos = reply.find(key, pos)) != std::string::npos) {
+            reply.replace(pos, key.length(), reply);
+            pos += reply.length();
+        }
+    }
+    return reply;
+}
+
+//response = formatReply(1, vars);
