c96afc4ee9c6b848b93eaf2c05d3c535e4447696
Dlouu <135530996+Dlouu@users.noreply.github.com>
2025-08-15 20:28:11 +0200

    Dlou 15/08/25: NICK with NumReply

--- Command/commands/nick.cpp
+++ Command/commands/nick.cpp
@@ -1,21 +1,30 @@
 #include "./../../Command/Command.hpp"
 
 void	Command::nickCommand( const CommandData_t& data ) const {
-	std::string nickname;
-	nickname = data.message.substr( 5, data.message.length() );
-	// if ( nickname.() )
- 	// 	return ; // ERR_ERRONEUSNICKNAME
-	if ( nickname.empty() )
-		return sendReply( data.fd, ERR_NONICKNAMEGIVEN );
-	std::string	error = "";
+	std::string nickname = data.message.substr( 5, data.message.length() );
 
 	std::map< int, Client > clients = Server::getClients();
-
 	for (std::map<int, Client>::iterator it = clients.begin(); it != clients.end(); it++) {
 		std::cout << it->second.getNickname()  << std::endl;
 		if (it->second.getNickname() == nickname) {
 			return sendReply( data.fd, ERR_NICKNAMEINUSE );
 		}
 	}
+	if ( nickname.empty() )
+		return sendReply( data.fd, ERR_NONICKNAMEGIVEN );
+	//else if (invalid nickname)
+			// nickname is considered invalid if:
+			// It contains spaces.
+			// It contains disallowed special characters (varies per server, but often excludes , * ? ! @ . : # & at certain positions).
+			// It starts with a character not allowed (most servers require it to start with a letter or special symbol like _ or [ but not a number).
+			// It exceeds the maximum length (commonly 9 or 30 characters, depending on server config).
+			// It violates server-specific nickname rules. 
+		//return sendReply( data.fd, ERR_ERRONEUSNICKNAME );
+	//else
+		//if (client was not already registered) 
+			//register nickname in database
+			//client nick set == true
+		//else (server 000 :<oldnick>!<user>@<host> NICK :<newnick>)
+
 	Server::setNicknameByFD( data.fd, nickname );
 }
