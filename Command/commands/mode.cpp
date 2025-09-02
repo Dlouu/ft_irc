bbf774944507b95a7306ac14d52e7cf7742112cc
Dlouu <135530996+Dlouu@users.noreply.github.com>
2025-09-02 17:24:58 +0200

    Dlou: 02/09/25: test params

--- Command/commands/mode.cpp
+++ Command/commands/mode.cpp
@@ -9,7 +9,7 @@ void	Command::modeCommand( const CommandData_t& data ) const {
 	if (params.size() < 2) {
 		return sendReply( data.fd, ERR_NEEDMOREPARAMS );
 	}
-	else if (params[1].empty() || params[1][0] != '#') { // || !isChannelExist(params[1])
+	else if (params[1].empty() || params[1][0] != '#') { 	// || !isChannelExist(params[1]) a add
 		return sendReply( data.fd, ERR_NOSUCHCHANNEL );
 	}
 	else {
@@ -87,55 +87,8 @@ void	Command::modeCommand( const CommandData_t& data ) const {
 
 			default:
 				return sendReply( data.fd, ERR_UMODEUNKNOWNFLAG );
-
 		}
-
 	}
-	// if (client isn't in channel database)
-	// 	return sendReply( data.fd, ERR_NOTONCHANNEL );
-	// else if (only 1 param)
-	// 	if (param == channel database)
-	// 		return sendReply( data.fd, RPL_CHANNELMODEIS );
-	// 	else if (param == client nickname)
-	// 		return sendReply( data.fd, RPL_UMODEIS ); (+ client modes)
-	// 	else
-	// 		return sendReply( data.fd, ERR_NOSUCHCHANNEL );
-	// else if(!channel OR !flag/mods)
-	// 	return sendReply( data.fd, ERR_NEEDMOREPARAMS );
-	
-	// if (flags/modes != i OR j OR t OR o OR k OR l)
-	// 	return sendReply( data.fd, ERR_UMODEUNKNOWNFLAG );
-
-	// if (channel not in database)
-	// 	return sendReply( data.fd, ERR_NOSUCHCHANNEL );
-	// else if (client not OP)
-	// 	return sendReply( data.fd, ERR_CHANOPRIVSNEEDED );
-	// if (flag/mods == o AND !target)
-	// 	return sendReply( data.fd, ERR_NEEDMOREPARAMS );
-	// 	else if (target not in channel data)
-	// 		return sendReply( data.fd, ERR_NOTONCHANNEL );
-	// 	else if ("+o")
-	// 		promote target to OP + message saying "+o target" ON CHANNEL
-	// 	else if ("-o")
-	// 		demote target from OP + message saying "-o target" ON CHANNEL
-	// else if (flags/mods == i)
-	// 	message telling channel is +i or -i on channel
-	// else if (flags == t)
-	// 	message telling channel is +t or -t on channel
-	// else if (flag ==  k)
-	// 	if (flag == +k AND !pass)
-	// 		return sendReply( data.fd, ERR_NEEDMOREPARAMS );
-	// 	else if (flag == +k AND new pass == old pass)
-	// 		return sendReply( data.fd, ERR_KEYSET );
-	// 	else
-	// 		:irc.example.net MODE #chatroom +k secret
-	// else if (flag == l)
-	// 	if (+l AND !limit)
-	// 		return sendReply( data.fd, ERR_NEEDMOREPARAMS );
-	// 	else if ("+l")
-	// 		set user limit to limit : :irc.example.net MODE #chatroom +l 10
-	// 	else
-	// 		remove set limit 
 }
 
 	//NOTES :
@@ -186,21 +139,9 @@ void	Command::modeCommand( const CommandData_t& data ) const {
 		//else
 			//remove set limit 
 
-
-	//ERR_NOSUCHNICK ??
-	//ERR_UNKNOWNMODE meh
-
 /* MODE - Change the channel’s mode:
 · i: Set/remove Invite-only channel
 · t: Set/remove the restrictions of the TOPIC command to channel operators
 · k: Set/remove the channel key (password)
 · o: Give/take channel operator privilege
 · l: Set/remove the user limit to channel */
-
-// ░▒▓████████▓▒░▒▓███████▓▒░ ░▒▓██████▓▒░░▒▓███████▓▒░       ░▒▓███████▓▒░░▒▓█▓▒░░▒▓█▓▒░▒▓███████▓▒░  
-//    ░▒▓█▓▒░   ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░ 
-//    ░▒▓█▓▒░   ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░ 
-//    ░▒▓█▓▒░   ░▒▓███████▓▒░░▒▓█▓▒░░▒▓█▓▒░▒▓███████▓▒░       ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓███████▓▒░  
-//    ░▒▓█▓▒░   ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░             ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░ 
-//    ░▒▓█▓▒░   ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░             ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░ 
-//    ░▒▓█▓▒░   ░▒▓█▓▒░░▒▓█▓▒░░▒▓██████▓▒░░▒▓█▓▒░             ░▒▓███████▓▒░ ░▒▓██████▓▒░░▒▓█▓▒░░▒▓█▓▒░ 
