1c86d1481ab82e13aa04691181700b6cdf99605a
Dlouu <135530996+Dlouu@users.noreply.github.com>
2025-09-15 16:13:50 +0200

    Dlou: 15/09/25: Topic

--- Command/commands/topic.cpp
+++ Command/commands/topic.cpp
@@ -1,35 +1,39 @@
 #include "Command.hpp"
 
-/*
-Verifier si mon setTopic marche bien, voir si j'ai choke sur les n* des params
-Pour le moment c'est pas ouf Huhu
-*/
-
 void	Command::topicCommand( const CommandData_t& data ) const {
-	Client	&client = *Server::getClientByFD( data.fd );
-	Channel	*channel;
+	Server		*server		= Server::getInstance();
+	Client		&client		= *server->getClientByFD( data.fd );
+	Channel		*channel	= NULL;
 
 	std::vector<std::string> params = split( data.message, ' ' );
 
-	if (params.size() >= 2) {
-		channel = Server::getInstance()->getChannel( params[3] );
-		if (!channel) {
-			sendReply( data.fd, ERR_NOSUCHCHANNEL );
+	//check params and channel
+	if (params.size() < 2) {
+		g_vars[ "command" ] = params[0];
+		return sendReply( data.fd, ERR_NEEDMOREPARAMS );
+	}
+	else if (params.size() >= 2) {
+		g_vars[ "channel" ] = params[1];
+		if (server->isChannelExist( params[1] ) == false) {
+			return sendReply( data.fd, ERR_NOSUCHCHANNEL );
 		}
+		channel = server->getChannel( params[1] );
 	}
+
+	//manage topic
 	if (params.size() == 2) {
+		if (channel->getTopic().empty()) {
+			sendReply( data.fd, RPL_NOTOPIC );
+		}
 		sendReply( data.fd, RPL_TOPIC );
-	}
-	else if (params.size() >= 3 && params[2].empty() && channel->getTopic() == "") {
-		sendReply( data.fd, RPL_NOTOPIC ); // a verifier, la description est bizarre
-	} else if (channel->isTopicRestricted() && channel->isClientOperator( client )) {
+	} else if (channel->isTopicRestricted() && !channel->isClientOperator( client )) {
 		sendReply( data.fd, ERR_CHANOPRIVSNEEDED );
-	} else if (channel->isClientUser( client )) {
+	} else if (!channel->isClientUser( client )) {
 		sendReply( data.fd, ERR_NOTONCHANNEL );
 	} else {
-		channel->setTopic( client, params[2] );
-		LOGC( INFO ) << channel->getTopic();
+		channel->setTopic( client, params[2].substr(1, params[2].size()) );
 		g_vars[ "topic" ] = channel->getTopic();
+		channel->shareMessage( client, channel->getTopic(), "TOPIC" );
 		sendReply( data.fd, RPL_TOPIC );
 	}
 }
